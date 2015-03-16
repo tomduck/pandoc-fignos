0f82014171a9889f04c361e48a22b94306d17838
Thomas Duck <tomduck@tomduck.ca>
2015-03-15 22:01:49 -0300

    Linting.

--- setup.py
+++ setup.py
@@ -1,33 +1,33 @@
+""" setup.py - install script for pandoc-fignos."""
 
-from setuptools import setup, find_packages
+from setuptools import setup
 
-
-long_description = """\
+LONG_DESCRIPTION = """\
 pandoc-fignos is a pandoc filter for numbering figures and figure references.
 """
 
 setup(
-    name = 'pandoc-fignos',
-    version = '0.1',
+    name='pandoc-fignos',
+    version='0.1',
 
-    author = 'Thomas J. Duck',
-    author_email = 'tomduck@tomduck.ca',
-    description = 'Figure number filter for pandoc',
-    long_description=long_description,
-    license = 'GPL',
-    keywords = 'pandoc figure numbers filter',
+    author='Thomas J. Duck',
+    author_email='tomduck@tomduck.ca',
+    description='Figure number filter for pandoc',
+    long_description=LONG_DESCRIPTION,
+    license='GPL',
+    keywords='pandoc figure numbers filter',
     url='https://github.com/tomduck/pandoc-fignos',
 
     install_requires=['pandocfilters', 'pandoc-attributes'],
 
-    py_modules = ['pandoc_fignos'],
-    entry_points = { 'console_scripts':
-                     ['pandoc-fignos = pandoc_fignos:main'] },
+    py_modules=['pandoc_fignos'],
+    entry_points={'console_scripts':['pandoc-fignos = pandoc_fignos:main']},
 
-    classifiers = [
+    classifier=[
         'Development Status :: 3 - Alpha',
         'Intended Audience :: End Users/Desktop',
         'Environment :: Console',
         'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
-        'Programming Language :: Python' ]
+        'Programming Language :: Python'
+        ]
 )
