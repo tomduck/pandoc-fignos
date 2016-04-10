714211d3cbb15bc2da3267c84c0370c91cc94b23
Thomas Duck <tomduck@tomduck.ca>
2016-04-09 21:44:17 -0300

    Added copyright notice.

--- setup.py
+++ setup.py
@@ -1,5 +1,20 @@
 """setup.py - install script for pandoc-fignos."""
 
+# Copyright 2015, 2016 Thomas J. Duck.
+# All rights reserved.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, version 3.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 import os
 
 import ez_setup
