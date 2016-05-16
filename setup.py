1789fe54efe946aafa6db895e006cf9cd3caa3f1
Thomas Duck <tomduck@tomduck.ca>
2016-05-16 01:08:38 -0300

    Require pandocxnos>=0.2.

--- setup.py
+++ setup.py
@@ -107,7 +107,7 @@ setup(
     url='https://github.com/tomduck/pandoc-fignos',
     download_url='https://github.com/tomduck/pandoc-fignos/tarball/'+VERSION,
 
-    install_requires=['pandoc-xnos>=0.1',
+    install_requires=['pandoc-xnos>=0.2',
                       'pandocfilters',
                       'pandoc-attributes'],
 
