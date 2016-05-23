a3a0253d2add980cb6d896edd147f6ea39c5e1ba
Thomas Duck <tomduck@tomduck.ca>
2016-05-23 20:56:05 -0300

    Updated xnos version.

--- setup.py
+++ setup.py
@@ -107,7 +107,7 @@ setup(
     url='https://github.com/tomduck/pandoc-fignos',
     download_url='https://github.com/tomduck/pandoc-fignos/tarball/'+VERSION,
 
-    install_requires=['pandoc-xnos>=0.3',
+    install_requires=['pandoc-xnos>=0.4',
                       'pandocfilters',
                       'pandoc-attributes'],
 
