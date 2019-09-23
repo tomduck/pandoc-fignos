1ab7da01f0cc98aad401056b0eeaf0525352e614
Thomas Duck <tomduck@tomduck.ca>
2019-09-23 10:46:26 -0300

    Fixed and updated requirement.

--- setup.py
+++ setup.py
@@ -46,7 +46,7 @@ setup(
     download_url='https://github.com/tomduck/pandoc-fignos/tarball/' + \
                  __version__,
 
-    install_requires=['pandoc-xnos~=2.0.0'],
+    install_requires=['pandoc-xnos~=2.1'],
 
     py_modules=['pandoc_fignos'],
     entry_points={'console_scripts':['pandoc-fignos = pandoc_fignos:main']},
